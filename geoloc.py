ae5130f4b82789f5fbb8d8c1e13d8cb07750ed61
qjaaferrer <qjaaferrer@tip.edu.ph>
2021-10-27 12:41:47 +0000

    first commit

---
+++ geoloc.py
@@ -0,0 +1,13 @@
+import ipapi
+from flask import Flask, request, render_template
+ 
+app = Flask(__name__)
+ 
+@app.route('/', methods = ['GET', 'POST'])
+def ipAdd():
+    search = request.form.get('search')
+    data = ipapi.location(ip=search, output='json')
+    return render_template('index.html', data=data)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
