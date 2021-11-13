cae317c3b8e322aef542e0e05c05f3347d724259
Janotelo <jagj102999@gmail.com>
2021-11-13 09:10:49 +0000

    Fixing Update

--- geoloc.py
+++ geoloc.py
@@ -2,12 +2,16 @@ import ipapi
 from flask import Flask, request, render_template
  
 app = Flask(__name__)
- 
+
 @app.route('/', methods = ['GET', 'POST'])
+def main():
+    return render_template('welcome.html')
+
+@app.route('/index.html', methods = ['GET', 'POST'])
 def ipAdd():
     search = request.form.get('search')
     data = ipapi.location(ip=search, output='json')
     return render_template('index.html', data=data)
- 
+
 if __name__ == "__main__":
     app.run(debug=True)
